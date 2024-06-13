43d183c4586c9da8edee9af23bb63c38fda99cfc
ct <charleswtiffany@gmail.com>
2024-06-12 20:15:45 -0700

    i think i need sleep

--- client/src/scripts/nav.js
+++ client/src/scripts/nav.js
@@ -1,48 +1,43 @@
-let nav = document.getElementsByClassName('bigNav')
+let nav = document.getElementsByClassName('bigNav')[0];
 
 const bigNav = `<div><a href="./index.html">Home</a></div>
     <div><a href="./hairSpray.html">Hair Spray</a></div>
     <div><a href="./hairRemoval.html">Hair Removal</a></div>
     <div><a href="./detanglers.html">Detanglers</a></div>
-    <div><a href="./coloringAndDyes.html">Hair Coloring & Dyes</a></div>`
+    <div><a href="./coloringAndDyes.html">Hair Coloring & Dyes</a></div>`;
 
-    const smallNav = `<div class="dropdown">
-<img src="./client/assets/images/hamburger.png" id="dropdown"/>
-<div class="dropdown-content" id="myDropdown">
-    <a href="index.html">Home</a>
-    <a href="hairSpray.html">Hair Spray</a>
-    <a href="hairRemoval.html">Hair Removal</a>
-    <a href="detanglers.html">Detanglers</a>
-    <a href="coloringAndDyes.html">Hair Coloring & Dyes</a>
+const smallNav = `<div class="dropdown">
+    <img src="./client/assets/images/hamburger.png" id="dropdown"/>
+    <div class="dropdown-content" id="myDropdown">
+        <a href="index.html">Home</a>
+        <a href="hairSpray.html">Hair Spray</a>
+        <a href="hairRemoval.html">Hair Removal</a>
+        <a href="detanglers.html">Detanglers</a>
+        <a href="coloringAndDyes.html">Hair Coloring & Dyes</a>
     </div>
-  </div>`
+</div>`;
 
-if(window.innerWidth >= 800){
-    nav = bigNav
+if (window.innerWidth >= 800) {
+    nav.innerHTML = bigNav;
 } else {
-    nav = smallNav
-}
+    nav.innerHTML = smallNav;
 
-// Get references to the dropdown elements
-var dropdown = document.getElementById("dropdown");
-var dropdownContent = document.getElementById("myDropdown");
+    var dropdown = document.getElementById("dropdown");
+    var dropdownContent = document.getElementById("myDropdown");
 
-// Toggle dropdown content when clicking on the dropdown image
-if(nav === big)return;
-    
-dropdown.addEventListener('click', function() {
-    if (dropdownContent.style.display === "block") {
-        dropdownContent.style.display = "none";
-    } else {
-        dropdownContent.style.display = "block";
-    }
-});
-
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('#dropdown')) {
+    dropdown.addEventListener('click', function() {
         if (dropdownContent.style.display === "block") {
             dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
         }
-    }
-};
+    });
+
+    window.onclick = function(event) {
+        if (!event.target.matches('#dropdown')) {
+            if (dropdownContent.style.display === "block") {
+                dropdownContent.style.display = "none";
+            }
+        }
+    };
+}
