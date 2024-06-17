dafdce4d695e897cd8afac39ec0391552d81b48e
ct <charleswtiffany@gmail.com>
2024-06-17 16:28:53 -0700

    updated webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -85,6 +85,7 @@ module.exports = {
     new InjectManifest({
       swSrc: "./src-sw.js",
       swDest: "./src-sw.js",
+      maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
     }),
 
     new WebpackPwaManifest({
